29dca446ea90625a979896f2995a45196d833eb7
ANTH0RNY <101851338+ANTH0RNY@users.noreply.github.com>
2024-09-06 18:12:27 +0300

    Fixed build errors

--- src/utils/PreloadLCPImage.tsx
+++ src/utils/PreloadLCPImage.tsx
@@ -1,30 +1,30 @@
-import { Helmet } from "react-helmet-async"
-import { EasyData, HardData, MediumData } from "./levels"
+// import { Helmet } from "react-helmet-async"
+// import { EasyData, HardData, MediumData } from "./levels"
 
-type PropsType = {
-    src: string
-}
+// type PropsType = {
+//     src: string
+// }
 
-export function PreloadLCPImage({ src }: PropsType) {
-    return (
-        <Helmet>
-            <link rel="preload" as="image" href={src} />
-        </Helmet>
-    )
-}
+// export function PreloadLCPImage({ src }: PropsType) {
+//     return (
+//         <Helmet>
+//             <link rel="preload" as="image" href={src} />
+//         </Helmet>
+//     )
+// }
 
-export function PreloadLCPImages() {
-    return (
-        <>
-            {
-                EasyData.cards.map(c => <PreloadLCPImage src={c.img} key={c.id} />)
-            }
-            {
-                MediumData.cards.map(c => <PreloadLCPImage src={c.img} key={c.id} />)
-            }
-            {
-                HardData.cards.map(c => <PreloadLCPImage src={c.img} key={c.id} />)
-            }
-        </>
-    )
-}
+// export function PreloadLCPImages() {
+//     return (
+//         <>
+//             {
+//                 EasyData.cards.map(c => <PreloadLCPImage src={c.img} key={c.id} />)
+//             }
+//             {
+//                 MediumData.cards.map(c => <PreloadLCPImage src={c.img} key={c.id} />)
+//             }
+//             {
+//                 HardData.cards.map(c => <PreloadLCPImage src={c.img} key={c.id} />)
+//             }
+//         </>
+//     )
+// }
